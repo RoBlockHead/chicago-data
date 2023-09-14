b534473f5ee2c34a8883612d795f90a182eadd3d
RoBlockHead <git@tessa.ooo>
2023-09-13 19:35:59 -0500

    add filtered datasets

--- processor.js
+++ processor.js
@@ -1,6 +1,18 @@
-import fs from 'fs';
+const fs = require('fs');
 
-const _streetsData = fs.readFileSync("./chicagostreets.geo.json");
+const _streetsData = fs.readFileSync("./streetsmin.geo.json");
 
 const streetsData = JSON.parse(_streetsData);
 
+const streets = streetsData.features;
+
+const newContents = {
+    type: "FeatureCollection",
+    features: []
+}
+
+streets.forEach(street => {
+    if(street.properties.class == "99") newContents.features.push(street);
+});
+
+fs.writeFileSync("./unclassified.geo.json", JSON.stringify(newContents));
